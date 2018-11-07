3b216c661262997ac854a269d360fe4925ad28f1
严俊东 <1172482914@qq.com>
2018-11-07 15:51:00 +0800

    jpa + mysql 练习初始化

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # springboot-project
+
+## start
+
+初始 spring boot 项目。
+
+依赖： web
+
+## login
+
+登入注册案例
+
+## admin
+
+后台管理案例
+
+## demo-springboot-mysql
+
+mysql 调用案例
+
+## payroll
+
+RESTful 练习
+
+人员工资
--- demo-springboot-mysql/src/main/java/com/example/demo/UserRepository.java
+++ demo-springboot-mysql/src/main/java/com/example/demo/UserRepository.java
@@ -7,9 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
